1ae002b8650bcdaf163d013cc4181bcaefbade29
luke <luke.nadackapadam@gmail.com>
2023-06-27 14:26:37 -0500

    changed item two

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,5 @@
 cell_phone = { :brand => "Apple", :color => "red", :price => 1000 }
 
-video_game = { title: "Skyrim", price: 60, developer: "Bethesda Game Studios" }
+candy = { brand: "Nerds", color: "Pink", price: 2 }
 
 soap = { brand: "Dial", color: "blue", price: 5 }
