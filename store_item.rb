3fd46725088b79a265cc7aceb0cd5db8399fef93
luke <luke.nadackapadam@gmail.com>
2023-06-27 14:23:27 -0500

    added third item

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,5 @@
 cell_phone = { :brand => "Apple", :color => "red", :price => 1000 }
 
 video_game = { title: "Skyrim", price: 60, developer: "Bethesda Game Studios" }
+
+soap = { brand: "Dial", color: "blue", price: 5 }
