724c546c27dcf86bb187c03509ece60b2489aa93
luke <luke.nadackapadam@gmail.com>
2023-06-28 13:42:39 -0500

    set Bike class to inherit from Car class

---
+++ inheritance_example.rb
@@ -0,0 +1,35 @@
+class Car
+  def initialize
+    @speed = 0
+    @direction = "north"
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Car
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+car1 = Car.new
+bike1 = Bike.new
+
+p car1.accelerate
+car1.honk_horn
+bike1.ring_bell
--- store_item.rb
+++ store_item.rb
@@ -40,4 +40,6 @@ item1 = Item.new({ brand: "Apple", color: "Red", price: 1000 })
 p item1.brand
 p item1.color
 item1.info
-p item1.price = 1200
+item1.price = 1200
+p item1.price
+item1.info
