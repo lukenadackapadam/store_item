38f64ce8617e0c274707388949a17ae2d31512c4
luke <luke.nadackapadam@gmail.com>
2023-06-28 12:30:22 -0500

    added a dollar sign

--- store_item.rb
+++ store_item.rb
@@ -28,7 +28,7 @@ class Item
   end
 
   def info
-    puts "The brand is #{brand}, the color is #{color}, and the price is #{price}."
+    puts "The brand is #{brand}, the color is #{color}, and the price is $#{price}."
   end
 end
 
