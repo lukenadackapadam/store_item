e48b0d91c85fadf8054234d2d816200eeda4b4ca
luke <luke.nadackapadam@gmail.com>
2023-06-28 11:40:23 -0500

    class init

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,13 @@
-cell_phone = { :brand => "Apple", :color => "red", :price => 1000 }
+# cell_phone = { :brand => "Apple", :color => "red", :price => 1000 }
 
-candy = { brand: "Nerds", color: "Pink", price: 2 }
+# candy = { brand: "Nerds", color: "Pink", price: 2 }
 
-soap = { brand: "Dial", color: "blue", price: 5 }
+# soap = { brand: "Dial", color: "blue", price: 5 }
+
+class Items
+  def initialize(input_brand, input_color, input_price)
+    @brand = input_brand
+    @color = input_color
+    @price = input_price
+  end
+end
